1d18b7ee9007766c52de4f8dab5cd30c2c4f24f2
Cyril Ricou <cyril.ricou@ambitiontelecom.com>
2017-02-22 18:52:20 +0000

    rdy

--- essai.py
+++ essai.py
@@ -1,5 +1,38 @@
-#coding: utf-8
-
-a = 1 *  2
-print "Hello Word"
-print 3.14 / a
+[1mdiff --git a/README.md b/README.md[m
+[1mdeleted file mode 100644[m
+[1mindex 1b842de..0000000[m
+[1m--- a/README.md[m
+[1m+++ /dev/null[m
+[36m@@ -1,18 +0,0 @@[m
+[31m-[m
+[31m-     ,-----.,--.                  ,--. ,---.   ,--.,------.  ,------.[m
+[31m-    '  .--./|  | ,---. ,--.,--. ,-|  || o   \  |  ||  .-.  \ |  .---'[m
+[31m-    |  |    |  || .-. ||  ||  |' .-. |`..'  |  |  ||  |  \  :|  `--, [m
+[31m-    '  '--'\|  |' '-' ''  ''  '\ `-' | .'  /   |  ||  '--'  /|  `---.[m
+[31m-     `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'[m
+[31m-    ----------------------------------------------------------------- [m
+[31m-[m
+[31m-[m
+[31m-Hi there! Welcome to Cloud9 IDE![m
+[31m-[m
+[31m-To get you started, create some files, play with the terminal,[m
+[31m-or visit http://docs.c9.io for our documentation.[m
+[31m-If you want, you can also go watch some training videos at[m
+[31m-http://www.youtube.com/user/c9ide.[m
+[31m-[m
+[31m-Happy coding![m
+[31m-The Cloud9 IDE team[m
+\ No newline at end of file[m
+[1mdiff --git a/essai.py b/essai.py[m
+[1mindex cbcc91d..9ac0f10 100644[m
+[1m--- a/essai.py[m
+[1m+++ b/essai.py[m
+[36m@@ -1,5 +1,4 @@[m
+ #coding: utf-8[m
+[31m-[m
+[31m-a = 1 *  2[m
+ print "Hello Word"[m
+[31m-print 3.14 / a[m
+\ No newline at end of file[m
+[41m+[m
+[41m+[m
